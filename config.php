81e76a2275437f1ef7b26a88276d996d6957b45d
takacsmarton <atakacsmarton@gmail.com>
2016-03-30 13:22:35 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 	error_reporting(0);
 	ini_set('display_errors', 0);
-	$mysql_host = "localhost";
-	$mysql_database = "miniblog";
-	$mysql_user = "miniblog";
-	$mysql_password = "Miniblog00";
+	$mysql_host = "";
+	$mysql_database = "";
+	$mysql_user = "";
+	$mysql_password = "";
 	$connect = mysqli_connect($mysql_host, $mysql_user, $mysql_password, $mysql_database);
 	mysqli_query($connect, "SET NAMES utf8");
 ?>
